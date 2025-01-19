8612730d321c189b495a7a8c41c17e73404908ae
Lucas Candido <candidolucas1993@gmail.com>
2025-01-18 18:36:33 -0800

    last

--- contact-form-main/script.js
+++ contact-form-main/script.js
@@ -24,8 +24,20 @@ form.addEventListener('submit', (e) => {
 
         // Insert error message after the input field
         firstname.insertAdjacentElement("afterend", errorMessage);
+    } 
+    if(lastname.value.trim() === '' || lastname.value.trim() == null){
+        const errorMessage = document.createElement('div');
+        errorMessage.className = 'error-message';
+        errorMessage.textContent = 'This field is required';
+
+        //Style div
+        errorMessage.style.color = 'red';
+        errorMessage.style.marginTop = '5px';
+
+        // Insert error message after the input field
         lastname.insertAdjacentElement("afterend", errorMessage);
-    } else{
+    
+    }else{
         alert('Form submitted successfuly!');
     }
 });
