20e6da77b992327f4a33090f199119a49721ec0e
Lucas Candido <candidolucas1993@gmail.com>
2025-01-21 19:11:51 -0800

    form_verification_error_message

--- contact-form-main/script.js
+++ contact-form-main/script.js
@@ -2,6 +2,12 @@ const form = document.getElementById('form_container');
 const firstname = document.getElementById('firstname');
 const lastname = document.getElementById('lastname');
 const email = document.getElementById('email');
+const enquiry = document.getElementById('generalenquiry');
+const request = document.getElementById('supportrequest');
+const error = document.getElementById('querytypeerror');
+const textarea = document.getElementById('paragraph_text');
+const checkbox = document.getElementById('checkbox');
+const checkboxlabel = document.getElementById('checkboxlabel');
 
 form.addEventListener('submit', (e) => {
 
@@ -38,7 +44,7 @@ form.addEventListener('submit', (e) => {
         // Insert error message after the input field
         lastname.insertAdjacentElement("afterend", errorMessage);
     
-    } else if(email.includes('@') && email.includes('.')){
+    } else if(email.value === '' || !email.value.includes('@')){
         const errorMessage = document.createElement('div');
         errorMessage.className = 'error-message';
         errorMessage.textContent = 'Please enter a valid email address';
@@ -50,7 +56,45 @@ form.addEventListener('submit', (e) => {
         // Insert error message after the input field
         email.insertAdjacentElement("afterend", errorMessage);
     
-    } else{
+    } else if(!enquiry.checked && !request.checked){
+        const errorMessage = document.createElement('div');
+        errorMessage.className = 'error-message';
+        errorMessage.textContent = 'Please select a query type';
+
+        //Style div
+        errorMessage.style.color = 'red';
+        errorMessage.style.marginBottom = '5px';
+
+        // Insert error message after the input field
+        error.insertAdjacentElement("afterend", errorMessage);
+    
+    } else if(textarea.value.trim() === '' || textarea.value.trim() == null){
+        const errorMessage = document.createElement('div');
+        errorMessage.className = 'error-message';
+        errorMessage.textContent = 'This field is required';
+
+        //Style div
+        errorMessage.style.color = 'red';
+        errorMessage.style.marginTop = '5px';
+
+        // Insert error message after the input field
+        textarea.insertAdjacentElement("afterend", errorMessage);
+
+    } else if(!checkbox.checked){
+        const errorMessage = document.createElement('div');
+        errorMessage.className = 'error-message';
+        errorMessage.textContent = 'Please select a query type';
+
+        //Style div
+        errorMessage.style.color = 'red';
+        errorMessage.style.marginBottom = '5px';
+
+        // Insert error message after the input field
+        checkboxlabel.insertAdjacentElement("afterend", errorMessage);
+    
+    }
+
+    else{
         alert('Form submitted successfuly!');
     }
 });
